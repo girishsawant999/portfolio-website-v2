64e61aa0bea2c76606164e3fe591bc110715eea8
Girish Sawant <girishsawant999.gs@gmail.com>
2025-05-25 15:14:33 +0530

    Add project details and media for StampMyVisa; update dependencies

---
+++ src/app/projects/constant.tsx
@@ -0,0 +1,34 @@
+export const PROJECTS = [
+  {
+    key: "stampmyvisa-biz",
+    title: "StampMyVisa - Biz",
+    description:
+      "An enterprise product designed to simplify visa processing, empowering travel agents to streamline and grow their business.",
+    skills: [
+      "React.js",
+      "TypeScript",
+      "Tailwind CSS",
+      "Figma",
+      "Ant Design",
+      "REST API",
+    ],
+    link: "https://stampmyvisa.com",
+    image: "/images/stampmyvisa.png",
+  },
+  {
+    key: "project-a",
+    title: "Project B",
+    description: "This is a description of Project B.",
+    skills: ["Next.js", "TypeScript", "Tailwind CSS"],
+    link: "https://example.com/project-b",
+    image: "/images/project-b.png",
+  },
+  {
+    key: "project-b",
+    title: "Project C",
+    description: "This is a description of Project C.",
+    skills: ["Next.js", "TypeScript", "Tailwind CSS"],
+    link: "https://example.com/project-c",
+    image: "/images/project-c.png",
+  },
+];
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,13 @@
+import clsx from "clsx";
+import { PROJECTS } from "./constant";
+
 const Projects = () => {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-      {/* Hero Section */}
+    <>
       <section className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-24">
         <div className="flex flex-col justify-center">
           <h1 className="text-6xl font-medium text-[#181717] mb-8">
-            Hello, I&apos;m Mehmet Akif.
+            Recent Work and Projects
           </h1>
           <div className="flex justify-start mt-4">
             <svg
@@ -24,84 +26,74 @@ const Projects = () => {
           </div>
         </div>
         <div className="flex flex-col justify-center">
-          <p className="text-[32px] text-[#606060]">
-            A passionate senior-year design student specializing in 3D modeling
-            and texturing, constantly exploring new techniques and creative
-            possibilities.
+          <p className="text-[32px] text-[#606060] body leading-9">
+            From concept to execution—here’s a look at what I’ve been building
+            lately. Each project reflects my focus on clean design, performance,
+            and user experience.
           </p>
         </div>
       </section>
 
-      {/* Project 1 */}
-      <section className="mb-16">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 mb-4">
-          <div className="lg:col-span-5 flex flex-col space-y-4">
-            <h2 className="text-lg font-medium text-[#181717]">
-              01 / PROJECT NAME
-            </h2>
-            <p className="text-lg text-[#181717]">
-              Goblin concept digital sculpture with human-like skin. Learned a
-              lot of quick tips and look development.
-            </p>
-            <p className="text-sm text-[#606060]">
-              Sculpted in ZBrush, Retopologized and UVs done in Maya, Textured
-              in Mari, Rendered using Arnold.
-            </p>
-            <div className="flex-grow"></div>
-            <a href="#" className="text-lg font-medium text-[#181717]">
-              More shots from this project ↗
-            </a>
+      {PROJECTS.map((project, index) => (
+        <section key={project.key} className="mb-16">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 mb-4">
+            <div className="lg:col-span-5 flex flex-col space-y-4">
+              <h2 className="text-lg font-medium text-[#181717]">
+                {String(index + 1).padStart(2, "0")} / {project.title}
+              </h2>
+              <p className="text-lg text-[#181717]">{project.description}</p>
+              {project.skills && (
+                <p className="text-sm text-[#606060]">
+                  {Array.isArray(project.skills) && project.skills.length > 0
+                    ? project.skills.join(", ")
+                    : project.skills}
+                </p>
+              )}
+              <div className="flex-grow"></div>
+              <a
+                href={project.link}
+                target="_blank"
+                className={clsx(
+                  "text-lg font-medium text-[#181717] flex items-center gap-1 relative w-fit",
+                  "before:content-[''] before:rounded-md before:h-px before:bottom-0.5 before:bg-current before:transition-[width] before:ease-in  before:absolute before:left-0 before:w-0",
+                  "hover:before:w-full"
+                )}
+              >
+                Preview this project
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="#000000"
+                  viewBox="0 0 256 256"
+                >
+                  <path d="M200,64V168a8,8,0,0,1-16,0V83.31L69.66,197.66a8,8,0,0,1-11.32-11.32L172.69,72H88a8,8,0,0,1,0-16H192A8,8,0,0,1,200,64Z"></path>
+                </svg>
+              </a>
+            </div>
+            <div className="lg:col-span-7">
+              <div className="w-full aspect-[16/9] bg-[#606060]">
+                <video
+                  className="w-full h-full object-cover"
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  poster={`/projects/${project.key}/image.png`}
+                >
+                  <source
+                    src={`/projects/${project.key}/video.mov`}
+                    type="video/mp4"
+                  />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            </div>
           </div>
-          <div className="lg:col-span-7">
-            <div className="w-full aspect-[16/9] bg-[#606060]"></div>
-          </div>
-        </div>
-        <div className="w-full border-b-[1.5px] border-[#181717] mt-6"></div>
-      </section>
-
-      {/* Project 2 */}
-      <section className="mb-16">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 mb-4">
-          <div className="lg:col-span-5 flex flex-col space-y-4">
-            <h2 className="text-lg font-medium text-[#181717]">
-              02 / PROJECT NAME
-            </h2>
-            <p className="text-lg text-[#181717]">
-              Subsurface weight, specular roughness, diffuse maps generated with
-              the procedure and manual techniques.
-            </p>
-            <div className="flex-grow"></div>
-            <a href="#" className="text-lg font-medium text-[#181717]">
-              See case study →
-            </a>
-          </div>
-          <div className="lg:col-span-7">
-            <div className="w-full aspect-[16/9] bg-[#606060]"></div>
-          </div>
-        </div>
-        <div className="w-full border-b-[1.5px] border-[#181717] mt-6"></div>
-      </section>
-
-      {/* Project 3 */}
-      <section className="mb-16">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 mb-4">
-          <div className="lg:col-span-5 flex flex-col space-y-4">
-            <h2 className="text-lg font-medium text-[#181717]">
-              03 / PROJECT NAME
-            </h2>
-            <p className="text-lg text-[#181717]">
-              Experimental creature modeling for school project.
-            </p>
-            <div className="flex-grow"></div>
-            <p className="text-lg font-medium text-[#606060]">Project WIP</p>
-          </div>
-          <div className="lg:col-span-7">
-            <div className="w-full aspect-[16/9] bg-[#606060]"></div>
-          </div>
-        </div>
-        <div className="w-full border-b-[1.5px] border-[#181717] mt-6"></div>
-      </section>
-    </div>
+          <div className="w-full border-b-[1.5px] border-[#181717] mt-6"></div>
+        </section>
+      ))}
+    </>
   );
 };
 
