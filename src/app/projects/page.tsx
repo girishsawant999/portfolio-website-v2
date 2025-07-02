f0b3d75153a66c218d223e042b7801b88d529d8b
Girish Sawant <girishsawant999.gs@gmail.com>
2025-07-02 16:22:05 +0530

    fix: enhance GSAP animations for project section and improve layout structure

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,11 +12,38 @@ gsap.registerPlugin(SplitText, ScrollTrigger);
 
 const Projects = () => {
   useGSAP(() => {
+    const tl = gsap.timeline();
+    const splitTextHeader = new SplitText(".project-page-title", {
+      type: "words",
+    });
+
     const splitTextDescription = new SplitText(".projects-page-description", {
       type: "words",
     });
 
-    gsap.from("article", {
+    tl.from(splitTextHeader.words, {
+      duration: 0.3,
+      y: 20,
+      opacity: 0,
+      stagger: 0.05,
+      ease: "power2.inOut",
+    });
+
+    tl.from(".projects-arrow-icon", {
+      duration: 0.3,
+      y: -40,
+      opacity: 0,
+      ease: "power2.inOut",
+    });
+
+    tl.from(splitTextDescription.words, {
+      duration: 0.2,
+      opacity: 0.2,
+      stagger: 0.05,
+      ease: "power2.inOut",
+    });
+
+    tl.from("article", {
       opacity: 0,
       y: 100,
       duration: 0.3,
@@ -25,13 +52,6 @@ const Projects = () => {
       onComplete: () => {
         gsap.set("article", { clearProps: "all" });
       },
-    });
-
-    gsap.from(splitTextDescription.words, {
-      duration: 0.2,
-      opacity: 0.2,
-      stagger: 0.05,
-      ease: "power2.inOut",
     });
 
     return () => {
@@ -43,9 +63,11 @@ const Projects = () => {
   return (
     <>
       <section className="grid grid-cols-1 lg:grid-cols-2 gap-8 mb-12 md:mb-24">
-        <div className="flex flex-col justify-center text-heading-1">
-          <h1 className="heading-1 mb-5 md:mb-8">Recent Work and Projects</h1>
-          <div className="flex justify-start mt-4">
+        <div className="flex flex-row md:flex-col items-center md:items-start justify-center text-heading-1 mb-5 md:mb-8 gap-5 md:gap-10">
+          <h1 className="project-page-title heading-1">
+            Recent Work and Projects
+          </h1>
+          <div className="flex justify-start">
             <svg
               width={34}
               height={39}
@@ -53,6 +75,7 @@ const Projects = () => {
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
               preserveAspectRatio="none"
+              className="projects-arrow-icon"
             >
               <path
                 d="M19.2187 0.181824H14.4176V29.2102L3.31959 18.1122L0.0326538 21.4361L16.8182 38.2216L33.6406 21.4361L30.2798 18.1122L19.2187 29.2102V0.181824Z"
